9ba93330ad0a76bf565d4b869f5c1673ba2e96e0
amcnevin <amcnevin@gmail.com>
2019-11-28 09:54:13 -0600

    init morse code translator

---
+++ morse_code.py
@@ -0,0 +1,94 @@
+import sys
+
+
+DOT = "."
+DASH = "-"
+WS = " "
+
+
+def translate_morse_code(input_str):
+    """
+    translate the input string into morse code
+    :param input_str: string to translate
+    """
+    morse = []
+    alphabet = get_alphabet()
+    for char in input_str:
+        morse.extend(convert_char(char, alphabet))
+
+    print(morse)
+
+
+def convert_char(input_char, alphabet):
+    """
+    convert a single character to morse code
+    :param input_char: character to convert
+    :param alphabet: cached alphabet
+    :return: List of DOTs and/or DASHes
+    """
+    return alphabet.get(input_char.upper(), WS)
+
+
+def get_alphabet():
+    """
+    build the alphabet
+    :return:
+    """
+    return {
+        "A": [DOT, DASH],
+        "B": [DASH, DOT, DOT],
+        "C": [DASH, DOT, DASH, DOT],
+        "D": [DASH, DOT, DOT],
+        "E": [DOT],
+        "F": [DOT, DOT, DASH, DOT],
+        "G": [DASH, DASH, DOT],
+        "H": [DOT, DOT, DOT, DOT],
+        "I": [DOT, DOT],
+        "J": [DOT, DASH, DASH, DASH],
+        "K": [DASH, DOT, DASH],
+        "L": [DOT, DASH, DOT, DOT],
+        "M": [DASH, DASH],
+        "N": [DASH, DOT],
+        "O": [DASH, DASH, DASH],
+        "P": [DOT, DASH, DASH, DOT],
+        "Q": [DASH, DASH, DOT, DASH],
+        "R": [DOT, DASH, DOT],
+        "S": [DOT, DOT, DOT],
+        "T": [DASH],
+        "U": [DOT, DOT, DASH],
+        "V": [DOT, DOT, DOT, DASH],
+        "W": [DOT, DASH, DASH],
+        "X": [DASH, DOT, DOT, DASH],
+        "Y": [DASH, DOT, DASH, DASH],
+        "Z": [DASH, DASH, DOT, DOT],
+        "0": [DASH, DASH, DASH, DASH, DASH],
+        "1": [DOT, DASH, DASH, DASH, DASH],
+        "2": [DOT, DOT, DASH, DASH, DASH],
+        "3": [DOT, DOT, DOT, DASH, DASH],
+        "4": [DOT, DOT, DOT, DOT, DASH],
+        "5": [DOT, DOT, DOT, DOT, DOT],
+        "6": [DASH, DOT, DOT, DOT, DOT],
+        "7": [DASH, DASH, DOT, DOT, DOT],
+        "8": [DASH, DASH, DASH, DOT, DOT],
+        "9": [DASH, DASH, DASH, DASH, DOT],
+        "&": [DOT, DASH, DOT, DOT, DOT],
+        "'": [DOT, DASH, DASH, DASH, DASH, DOT],
+        "@": [DOT, DASH, DASH, DOT, DASH, DOT],
+        ")": [DASH, DOT, DASH, DASH, DOT, DASH],
+        "(": [DASH, DOT, DASH, DASH, DOT],
+        ":": [DASH, DASH, DASH, DOT, DOT, DOT],
+        ",": [DASH, DASH, DOT, DOT, DASH, DASH],
+        "=": [DASH, DOT, DOT, DOT, DASH],
+        "!": [DASH, DOT, DASH, DOT, DASH, DASH],
+        ".": [DOT, DASH, DOT, DASH, DOT, DASH],
+        "-": [DASH, DOT, DOT, DOT, DOT, DASH],
+        "+": [DOT, DASH, DOT, DASH, DOT],
+        "?": [DOT, DOT, DASH, DASH, DOT, DOT],
+        "/": [DASH, DOT, DOT, DASH, DOT],
+    }
+
+
+if __name__ == "__main__":
+    translate_morse_code(sys.argv[1])
+
+
